6ecd1430ca6b7bdd5ea814bf7ab9f14098aa7d2f
Joa <Joa@DESKTOP-UM815T1>
2021-08-05 11:56:52 -0400

    Ultima version

--- src/main/java/EjercicioII.java
+++ src/main/java/EjercicioII.java
@@ -1,6 +1,6 @@
 public class EjercicioII {
     public static void main(String [] args){
-        Integer vector[]={1,2};
+        Integer vector[]={1,2,4,5,6,1,1,4,6,10,12,5,6,12};
         quicksort<Integer> VectorOrdenado= new quicksort<>();
         VectorOrdenado.qsort(vector, 0, vector.length-1);
         Remover<Integer> VectorSinduplicados= new Remover<>();
