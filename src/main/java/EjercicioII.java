241d1fa67068280163eb6794e9ffeb3877868cb1
Joa <Joa@DESKTOP-UM815T1>
2021-08-05 11:55:07 -0400

    Ultima version

--- src/main/java/EjercicioII.java
+++ src/main/java/EjercicioII.java
@@ -1,11 +1,10 @@
 public class EjercicioII {
     public static void main(String [] args){
-        Integer vector[]={1,2,3,3,3,5,1,4,5,7,8};
+        Integer vector[]={1,2};
         quicksort<Integer> VectorOrdenado= new quicksort<>();
         VectorOrdenado.qsort(vector, 0, vector.length-1);
         Remover<Integer> VectorSinduplicados= new Remover<>();
         int tam=VectorSinduplicados.remo(vector);
-        
         for (int i=0;i<tam;i++){
             System.out.println(vector[i]);
         }
