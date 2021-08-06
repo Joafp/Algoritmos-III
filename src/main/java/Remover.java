6f6a4bcf4b00e79382d8eea26f6a1e4e1ae1f4ab
Joa <Joa@DESKTOP-UM815T1>
2021-08-05 22:27:45 -0400

    Ultima version

--- src/main/java/Remover.java
+++ src/main/java/Remover.java
@@ -1,16 +1,27 @@
+/*Para poder encontrar los elementos repetidos de nuestro arreglo lo primero que hacemos es ordenarlo mediante un quicksort que tiene un tiempo de
+O(nlogn), una vez ordenado el vector lo que conseguimos es que los elementos repetidos queden uno al lado del otro. Con el arreglo ordenado ahora podemos colocar los que no son repetidos
+uno al lado del otro esto nos llevaria un tiempo de O(n), por lo que en total nuestro T(n)=O(nLog(n))+O(n), solo nos quedamos con el mayor por lo que al final nos quedaria que el tiempo es de 
+T(n)=O(nLog(n)).
+*/
 public class Remover <T extends Comparable<? super T>>{
     int remo(T []vector){
         quicksort<T> VectorOrdenado= new quicksort<>();
-        VectorOrdenado.qsort(vector, 0, vector.length-1);
-        int i=0,j=0;
-        while (i<vector.length-1){
-            vector[j]=vector[i];
-            while (vector[i].compareTo(vector[i+1])==0){
-                i+=1;
+        VectorOrdenado.qsort(vector, 0, vector.length-1);//O(nlog(n))
+        int tam=vector.length;
+        if (tam==1 || tam==0)
+            return tam;
+        T aux[]=vector;
+        int j=0,i;
+        for (i=0;i<tam-1;i++){//O(n)
+            if (vector[i].compareTo(vector[i+1])!=0){
+                aux[j++]=vector[i];
             }
-            i+=1;
-            j+=1;
         }
-        return j;
+        aux[j++]=vector[tam-1];
+        for (i=0;i<j;i++){//O(n)
+            vector[i]=aux[i];
+        }
+        return j;     
+        //sumando todo T(n)=O(nLog(n))+2O(n),que tomando el mayor es T(n)=O(nLog(n))
     }
 }
