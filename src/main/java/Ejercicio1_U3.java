c09b7f1c1a6aaaae42f14e85968937951c6e8a56
Joa <Joa@DESKTOP-UM815T1>
2021-08-09 09:30:33 -0400

    Ultima version

--- src/main/java/Ejercicio1_U3.java
+++ src/main/java/Ejercicio1_U3.java
@@ -1,6 +1,15 @@
-
 public class Ejercicio1_U3 {
     public static void main(String [] args){
-        System.out.println("Hola");
+        BST<String> t = new BST<String>();
+        String [] A = { "hola", "amor", "roma", "barco", "mesa", "musica", "zapato", "amigo"};
+        for ( int k=0; k < A.length; k++ )
+            t.agregar( A[k]);
+        t.imprimir();
+        String k = t.buscar("zapato");
+        System.out.println();
+        if ( k != null )
+            System.out.println("Si existe!! " + k);
+        else
+            System.out.println("No existe!!");
     }
 }
