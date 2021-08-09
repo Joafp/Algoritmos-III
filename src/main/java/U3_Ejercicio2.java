2f19954844ebedc0293ad31b0bdde5d2369997f6
Joa <Joa@DESKTOP-UM815T1>
2021-08-09 11:27:23 -0400

    Ultima version

--- src/main/java/U3_Ejercicio2.java
+++ src/main/java/U3_Ejercicio2.java
@@ -1,6 +1,11 @@
-
 public class U3_Ejercicio2 {
     public static void main(String []args){
-        System.out.println("Hola");
+        Integer []aux={10,15,7,8,6,2,11,12};
+        BST <Integer> v=new BST<Integer>();
+        for (int k=0;k<aux.length;k++){
+            v.agregar(aux[k]);
+        }
+        v.imprimirprofundidad(v.getraiz(), 0);
+        
     }
 }
