b5dc060ea38be90be69890eb414d94828b98dbea
Joa <Joa@DESKTOP-UM815T1>
2021-08-11 16:20:31 -0400

    Ultima version

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -1,13 +1,14 @@
-
-public class BST<T extends Comparable<? super T>> {
-    class nodo{
+import java.util.Iterator;
+import java.util.Stack;
+public class BST <T extends Comparable> implements Iterable <T>{
+    class nodo  {
         T elemento;
         nodo izq,der;
         public nodo(T elem){
             elemento=elem;
             izq=der=null;
         }
-    }
+     }    
     nodo raiz;
     BST(){
         raiz=null;
@@ -62,4 +63,40 @@ public class BST<T extends Comparable<? super T>> {
             inorder(raiz.der);
         }
     }
+    @Override
+    public Iterator <T> iterator(){
+        Iterator aux=new BSTIterator(raiz);
+        return aux;
+    }
+    protected class BSTIterator implements Iterator <T> {
+        Stack<nodo> stack = new Stack<nodo>();
+        public BSTIterator(nodo root) {
+            pushToLeft(root);
+        }
+        @Override
+        public boolean hasNext(){
+            if(stack.empty()){
+                return false;
+            }return true;
+        }
+        @Override
+        public T next(){
+            nodo node = stack.pop();
+            pushToLeft(node.der);
+            return node.elemento;
+        }
+        private void pushToLeft(nodo node){
+            if (node != null){
+                stack.push(node);
+                pushToLeft(node.izq);
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
--- src/main/java/Ejercicio1_U3.java
+++ src/main/java/Ejercicio1_U3.java
@@ -1,15 +1,18 @@
 public class Ejercicio1_U3 {
     public static void main(String [] args){
-        BST<String> t = new BST<String>();
+        BST<String> t = new BST<>();
         String [] A = { "hola", "amor", "roma", "barco", "mesa", "musica", "zapato", "amigo"};
         for ( int k=0; k < A.length; k++ )
             t.agregar( A[k]);
         t.imprimir();
         String k = t.buscar("zapato");
         System.out.println();
+        for (String s:t){
+            System.out.printf("%s ",s);
+        }
         if ( k != null )
-            System.out.println("Si existe!! " + k);
+            System.out.println("\nSi existe!! " + k);
         else
-            System.out.println("No existe!!");
+            System.out.println("No existe!!");     
     }
 }
