b9c9d6bb6f373b974f9c681d8cc6b6a1e940806b
Joa <Joa@DESKTOP-UM815T1>
2021-08-12 12:42:36 -0400

    Ultima version

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Stack;
 public class BST <T extends Comparable> implements Iterable <T>{
+    //Parte a del ejercicio
     class nodo  {
         T elemento;
         nodo izq,der;
@@ -13,14 +15,14 @@ public class BST <T extends Comparable> implements Iterable <T>{
     BST(){
         raiz=null;
     }
-    public T buscar(T valor){
+    public T buscar(T valor){//Para buscar no aplicamos la forma recursiva sino la iterativa
         if (raiz.elemento.compareTo(valor)==0){
-            return valor;
+            return valor;//si es la raiz podemos retornar directamente
         }else{
             nodo actual=raiz;
-            while (actual!=null){
+            while (actual!=null){//mientras no sea nulo seguimos el cilo, si es que en algun momento el nodo es nulo es pq el elemento no se encuentra en el arbol
                 if (actual.elemento.compareTo(valor)>0){
-                    actual=actual.izq;
+                    actual=actual.izq;//atctualizamos el nodo en caso de que sea menor o mayor
                 }else if(actual.elemento.compareTo(valor)<0){
                     actual=actual.der;
                 }else{
@@ -30,14 +32,15 @@ public class BST <T extends Comparable> implements Iterable <T>{
         }
         return null;
     }
-    public void agregar(T v){
+    public void agregar(T v){//Para agregar tambien usamos un metodo iterativo
         nodo nuevo=new nodo(v);
-        if (raiz==null){
+        if (raiz==null){//en caso de que la raiz este vacia añadimos directamente
             raiz=nuevo;
             return;
         }
-        nodo anterior=null;
+        nodo anterior=null;// nos servira en el momento de asignar si es hijo izq o der del padre
         nodo actual=raiz;
+        //ciclo donde encontramos el lugar donde tiene que ubicarse el nodo
         while (actual!=null){
             if (actual.elemento.compareTo(v)>0){
                 anterior=actual;
@@ -53,6 +56,7 @@ public class BST <T extends Comparable> implements Iterable <T>{
             anterior.der=nuevo;
         }   
     }
+    //para imprimor usamos un inorder
     public void imprimir(){
         inorder(raiz);
     }
@@ -63,34 +67,64 @@ public class BST <T extends Comparable> implements Iterable <T>{
             inorder(raiz.der);
         }
     }
+    //Parte b del ejercicio
+    //Para poder usar la estructura del arbol en un for each lo que hicimos fue implementar iterable a nuestra clase
     @Override
+    //El iterator que nos servira en el for each
     public Iterator <T> iterator(){
         Iterator aux=new BSTIterator(raiz);
         return aux;
     }
+    //Para poder hacer la clase del Iterator nos ayudamos de esta pagina https://medium.com/algorithm-problems/binary-search-tree-iterator-19615ec585a
     protected class BSTIterator implements Iterator <T> {
-        Stack<nodo> stack = new Stack<nodo>();
+        Stack<nodo> stack = new Stack<nodo>();//para nuestra conveniencia usamos un stack
         public BSTIterator(nodo root) {
-            pushToLeft(root);
+            enviarizq(root);// añadimos al stack
         }
         @Override
-        public boolean hasNext(){
+        public boolean hasNext(){// en caso de que nuestra stack ya este vacio
             if(stack.empty()){
                 return false;
             }return true;
         }
         @Override
-        public T next(){
+        public T next(){// para comprobar que sigue teniendo elementos nuestro stack
             nodo node = stack.pop();
-            pushToLeft(node.der);
+            enviarizq(node.der);
             return node.elemento;
         }
-        private void pushToLeft(nodo node){
+        private void enviarizq(nodo node){
             if (node != null){
                 stack.push(node);
-                pushToLeft(node.izq);
+                enviarizq(node.izq);
             }
         }
+    }
+    //Parte c del ejercicio
+    //Creamos un inorder que añade elementos en un arraylist estos elementos se guardan de forma ordenada
+    void inorderañadir(nodo raiz,ArrayList aux){
+        if (raiz!=null){
+            inorderañadir(raiz.izq,aux);
+            aux.add(raiz.elemento);
+            inorderañadir(raiz.der,aux);
+        }
+    } 
+    //Para poder retornar un iterable debemos usar un array list
+    public Iterable <T> iteratorClaves(T min,T max){
+        //En caso de que la clave minima es mayor debe lanzar un error
+        if (min.compareTo(max)>0){
+            System.out.println("La clave minima es menor a la maxima");
+            return null;
+        }
+        ArrayList<T> aux= new ArrayList<T>();
+        inorderañadir(raiz,aux);
+        int pos1=aux.indexOf(min);
+        int pos2=aux.indexOf(max);
+        ArrayList<T> aux2= new ArrayList<T>();//Nuestro segundo array contiene solo los elementos desde la clave minima hasta la maxima
+        for (int i=pos1;i<=pos2;i++){
+            aux2.add(aux.get(i));
+        }
+        return aux2;
     }
 }
 
