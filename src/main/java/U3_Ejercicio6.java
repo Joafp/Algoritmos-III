72fe0159273e9f0caf073573a6ac4129a0d0ccbe
Joaquin <88462514+Joafp@users.noreply.github.com>
2021-08-13 15:06:30 -0400

    Update U3_Ejercicio6.java

--- src/main/java/U3_Ejercicio6.java
+++ src/main/java/U3_Ejercicio6.java
@@ -1,14 +1,14 @@
 public class U3_Ejercicio6 {
     public static void main (String []arg){
-        String [] v={"Hola","Chau","Que tal","Todo bien","Alo","HEHE","Que"};
-        BST<String> t=new BST<>();
+        Integer [] v={21,10,71,15,13,16,71,28};
+        BST<Integer> t=new BST<>();
         for (int i=0;i<v.length;i++){
             t.agregar(v[i]);
         }
         t.imprimir();
         System.out.println(t.conthojas());
-        t.eliminar("Alo");
         t.imprimir();
+        System.out.println(t.encontrarsucesor(t.getraiz(),null,10));
         
     }
 }
