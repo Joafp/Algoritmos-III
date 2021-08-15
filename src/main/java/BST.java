10055448db4a64343d19f538255e660213eede11
Joaquin <88462514+Joafp@users.noreply.github.com>
2021-08-15 09:43:55 -0400

    Update BST.java

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -1,3 +1,4 @@
+//Integrantes Joaquin Delgado, Carlos Ayala
 public class BST<T extends Comparable>
 {
    private class NodoBST { 
@@ -24,6 +25,10 @@ public class BST<T extends Comparable>
    /* Agregar un dato al arbol */ 
    public void agregar (T dato)
    {
+      if (buscar(dato)!=null){
+          System.out.println("El "+dato+" ya existe en el arbol");
+          return;
+      }
       raiz = priv_agregar (raiz, dato);
    }
 
@@ -37,10 +42,7 @@ public class BST<T extends Comparable>
         NodoBST nodo = priv_buscar(raiz,dato);
             if ( nodo != null ) 
                 return nodo;
-            else { /* Reemplazar por manejo de excepcion!! */
-                    System.out.println("No existe en el arbol!!! " + dato);
-            return null;	  
-            }	 
+          return null;	 
    }
 
    /*
@@ -57,11 +59,13 @@ public class BST<T extends Comparable>
    }
    //Eliminar Iterativo
    private void eliminar_iterativo(T dato){
+        //Para eliminar la raiz solo hay que ponerla nula
         if (raiz.dato.compareTo(dato)==0){
             System.out.println("Se elimino la raiz");
             raiz=null;
             return;
         }
+        //En caso de que no sea la raiz lo que debemos de hacer es ver si lo que queremos eliminar se encuentra en nuestro arbol
         NodoBST actual=raiz;
         NodoBST anterior=null;
         boolean bandera=false;
@@ -76,10 +80,13 @@ public class BST<T extends Comparable>
                 bandera=true;
             }
         }
+        //En caso de que actual fuera null significa que el elemento no se encuentra en el arbol
         if (actual==null){
             System.out.println("No se encuentra lo que se quiere eliminar");
             return;
         }else{
+            //Una vez encontrado el nodo debemos de analizar los siguientes casos
+            //Primer caso es que el nodo sea una hoja, cuando pasa esto simplemente eliminamos el nodo
             if (actual.izq==null && actual.der==null){
                 if (anterior.izq==actual){
                     anterior.izq=null;
@@ -87,9 +94,9 @@ public class BST<T extends Comparable>
                     anterior.der=null;
                 }
                 actual=null;
-            }else if (actual.izq!=null && actual.der==null){
+            }else if (actual.izq!=null && actual.der==null){//El sigueinte caso es que el nodo tenga un hijo izq o derecho
                 if (anterior.izq==actual){
-                    anterior.izq=actual.getizq();
+                    anterior.izq=actual.getizq();//Simplemente vemos si el nodo es hijo izq o der del padre y ahora decimos que el hijo del nodo es hijo del padre
                 }else{
                     anterior.der=actual.getizq();
                 }
@@ -102,8 +109,10 @@ public class BST<T extends Comparable>
                 }
                 actual=null;
             }else{
+                //Por ultimo nos queda el caso donde el nodo tiene 2 hijos, en nuestro caso nosotros decidimos reemplazar el nodo por su sucesor
                 NodoBST sucesor=null;
                 NodoBST padresucesor=null;
+                //Pirmero encontramos el sucesor
                 if (actual.der!=null){ 
                     sucesor=actual;
                     while (sucesor.izq!=null){
@@ -123,19 +132,22 @@ public class BST<T extends Comparable>
                         }
                     }
                 }
-                if (actual.izq!=null && sucesor!=actual.izq){
-                    sucesor.izq=actual.izq;
-                }
-                if (actual.der!=null&&  sucesor!=actual.der){
-                    sucesor.der=actual.der;
-                }
+                //En caso de que el sucesor no tenga ningun hijo simplemente cambiamos los punteros para que ahora el padre del nodo que queremos eliminar apunto al sucesor
                 if (sucesor.der==null){
                     if (anterior.izq==actual){
                         anterior.izq=sucesor;
                     }else{
                         anterior.der=sucesor;
                     }
+                    //Le cambiamos los nodos hijos del que queremos eliminar al sucesor
+                    if (actual.izq!=null && sucesor!=actual.izq){
+                        sucesor.izq=actual.izq;
+                    }
+                    if (actual.der!=null&&  sucesor!=actual.der){
+                        sucesor.der=actual.der;
+                    }
                 }else{
+                    //En caso de que el sucesor tenga hijos le asignamos esos hijos al padre del sucesor
                     if (padresucesor.izq==sucesor){
                         padresucesor.izq=sucesor.der;
                     }else{
@@ -146,7 +158,15 @@ public class BST<T extends Comparable>
                     }else{
                         anterior.der=sucesor;
                     }
+                     //Le cambiamos los nodos hijos del que queremos eliminar al sucesor
+                    if (actual.izq!=null && sucesor!=actual.izq){
+                        sucesor.izq=actual.izq;
+                    }
+                    if (actual.der!=null&&  sucesor!=actual.der){
+                        sucesor.der=actual.der;
+                    }
                 }
+              
             }
         }
         
@@ -156,11 +176,46 @@ public class BST<T extends Comparable>
         NodoBST aux=private_encontrarsucesor(n,suce,dato);
         return aux.dato;
     }
+    public boolean lleno(NodoBST n){
+        return private_lleno(n);
+    } 
+    private boolean private_lleno(NodoBST n){//Para ver si el nodo esta lleno recorremos recursivamente el arbol
+        if (n==null){
+            return true;
+        }
+        if (n.izq==null && n.der==null){
+            return true;
+        }
+        if (n.izq!=null && n.der!=null){
+            return private_lleno(n.izq) && private_lleno(n.der);
+        }
+        return false;
+    }
+    public int contnodos(NodoBST n){
+        return private_contnodos(n);
+    }
+    private int private_contnodos(NodoBST n){
+        if (n==null){
+            return 0;
+        }
+        return 1+contnodos(n.izq)+ contnodos(n.der);//Para el contador de nodo recorremos los nodos y sumamos 1 cada vez que encontramos un nodo y 0 si es un nodo null
+    }
+    public boolean completo(NodoBST n,int pos,int cantidad){
+        if (n==null){
+            return true;
+        }
+        if (pos>=cantidad){
+            return false;
+        }
+        return completo(n.izq,2*pos+1,cantidad) && completo(n.der,2*pos+2,cantidad);//Para ver si esta completo tenemos pondremos posiciones a los nodos, el nodo raiz es 0 sus hijos son 1 y 2
+                                                                                    //Los hijos de 1 seran 3 y 4 y de 2 5 y 6, asi sucesivamente si es que encontramos un una posicion que es mayor que la cantidad de nodos
+                                                                                    //es por que el arbol no esta completo
+    }
     private NodoBST private_encontrarsucesor(NodoBST n,NodoBST suce,T dato){
         if (n==null){
             return suce;
         }
-        if (n.dato.compareTo(dato)==0){
+        if (n.dato.compareTo(dato)==0){//Para encontrar el sucesor simplemente vemos si nuestro nodo tiene surarbol derecho, en caso de que tenga buscamos el valor minimo
             if (n.der!=null){
                 return valorminimo(n.der);
             }
@@ -180,7 +235,7 @@ public class BST<T extends Comparable>
             if (n==null){
                 return prede;
             }
-            if (n.dato.compareTo(dato)==0){
+            if (n.dato.compareTo(dato)==0){//Para el predecesor hacemos lo mismo solo que buscamos el valor maximo
                 if (n.izq!=null){
                     return valormaximo(n.izq);
                 }
@@ -188,18 +243,18 @@ public class BST<T extends Comparable>
                 return private_encontrarpredecesor(n.izq,prede,dato);
             }else{
                 prede=n;
-                return private_encontrarpredecesor(n.der,prede,dato);
+                return private_encontrarpredecesor(n.der,prede,dato);//En caso de que el valor que buscamos es mayor que nuestro nodo, buscamos el prede de la derecaha
             }
             return prede; 
     }
-    private NodoBST valorminimo(NodoBST n){
+    private NodoBST valorminimo(NodoBST n){//Simplemente un recorrido de la parte izq de un subarbol
         NodoBST aux=n;
         while (aux.izq!=null){
             aux=aux.izq;
         }
         return aux;
     }
-    private NodoBST valormaximo(NodoBST n){
+    private NodoBST valormaximo(NodoBST n){//Recorrido de la parte derecha de un subarbol
         while(n.der!=null){
             n=n.getder();
         }
@@ -210,7 +265,7 @@ public class BST<T extends Comparable>
    {
       if ( n_actual == null )
          return ( new NodoBST(dato) );
-
+      
      int comparacion = dato.compareTo (n_actual.dato);
 	 
 	 if ( comparacion < 0 ) 
